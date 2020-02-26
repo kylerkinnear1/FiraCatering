f062aff1ede41e3ed7c57915ed07e998ecdef679
kylerkinnear1 <38384166+kylerkinnear1@users.noreply.github.com>
2020-02-26 02:42:39 -0600

    Refactored packages into a master component.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,38 +1,19 @@
 <template>
   <div>
     <header-highlights />
-    <div class="packages-container">
-      <div>
-        <featured-package title="Basic" price="$7.99" />
-      </div>
-      <div>
-        <featured-package title="Classic" price="$9.99" />
-      </div>
-      <div>
-        <featured-package title="Deluxe" price="$21.99" />
-      </div>
-    </div>
+    <featured-packages />
   </div>
 </template>
 
 <script>
 import HeaderHighlights from "@/components/HeaderHighlights.vue";
-import FeaturedPackage from "@/components/FeaturedPackage.vue";
+import FeaturedPackages from "@/components/FeaturedPackages.vue";
 
 export default {
   name: "home-page",
-  components: { HeaderHighlights, FeaturedPackage }
+  components: { HeaderHighlights, FeaturedPackages }
 };
 </script>
 
 <style scoped>
-.packages-container {
-  margin: 0px 0px 200px 0px;
-  display: flex;
-  justify-content: center;
-}
-
-.packages-container div {
-  margin: 0px 2px;
-}
 </style>
