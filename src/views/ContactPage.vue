32acbf2e947ff4c2ec9f7d0178fff9a9f2e12b48
kylerkinnear1 <38384166+kylerkinnear1@users.noreply.github.com>
2020-02-27 07:32:47 -0600

    Committing progress on Contact page. Quite a big of work / cleanup needs to be done.

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,5 +1,16 @@
 <template>
-  <div />
+  <div class="container">
+    <div class="contact-card">
+      <h1>Contact Us</h1>
+      <div class="contact-card-container">
+        <img src="@/assets/images/contact-us-highlight.jpg" />
+        <form>
+          <label for="customer-name">Name</label>
+          <input type="text" id="customer-name" />
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +19,45 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  background-image: url("~@/assets/images/contact-us-background.jpg");
+  background-repeat: no-repeat;
+  background-size: contain;
+  padding-top: var(--nav-bar-height);
+  padding: 800px 100px 100px 100px;
+}
+
+.contact-card {
+  background: white;
+  text-align: center;
+}
+h1 {
+  text-transform: uppercase;
+  font-family: "Roboto";
+  font-weight: lighter;
+  padding: 40px;
+  font-size: 40pt;
+}
+
+img {
+  transform: scale(0.62);
+  transform-origin: 0 0;
+}
+
+.contact-card-container {
+  display: flex;
+  justify-content: center;
+}
+
+label {
+  font-family: "Roboto";
+  font-weight: lighter;
+  text-transform: uppercase;
+  display: block;
+}
+
+input {
+  display: block;
+}
+</style>
