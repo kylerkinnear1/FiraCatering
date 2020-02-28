c84704d8ba3111dea5e426903a3fe1257b1c7c22
kylerkinnear1 <38384166+kylerkinnear1@users.noreply.github.com>
2020-02-27 20:50:41 -0600

    Got the about page 'functional' enough for now. The markup / css is pretty bad though and needs a lot of cleanup.

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -5,8 +5,32 @@
       <div class="contact-card-container">
         <img src="@/assets/images/contact-us-highlight.jpg" />
         <form>
-          <label for="customer-name">Name</label>
-          <input type="text" id="customer-name" />
+          <div>
+            <label for="customer-name">Name *</label>
+            <input type="text" id="customer-name" />
+          </div>
+          <div>
+            <label for="customer-email">Email *</label>
+            <input type="text" id="customer-email" />
+          </div>
+          <div>
+            <label for="customer-phone">Phone</label>
+            <input type="text" id="customer-phone" />
+          </div>
+          <div class="share-line">
+            <div class="share-line-item">
+              <label for="event-type">Event Type</label>
+              <input type="text" id="event-type" />
+            </div>
+            <div class="share-line-item">
+              <label for="event-date">Event Date *</label>
+              <input type="text" id="event-date" />
+            </div>
+          </div>
+          <div>
+            <label for="comments">Comments</label>
+            <textarea type="text" id="comments" />
+          </div>
         </form>
       </div>
     </div>
@@ -25,7 +49,7 @@ export default {
   background-repeat: no-repeat;
   background-size: contain;
   padding-top: var(--nav-bar-height);
-  padding: 800px 100px 100px 100px;
+  padding: 750px 200px 20px 200px;
 }
 
 .contact-card {
@@ -41,13 +65,14 @@ h1 {
 }
 
 img {
-  transform: scale(0.62);
-  transform-origin: 0 0;
+  width: 583px;
+  height: 437px;
+  margin-left: 60px;
+  margin-bottom: 100px;
 }
 
 .contact-card-container {
   display: flex;
-  justify-content: center;
 }
 
 label {
@@ -55,9 +80,51 @@ label {
   font-weight: lighter;
   text-transform: uppercase;
   display: block;
+  margin-bottom: 6px;
+  font-size: 16pt;
+  text-align: left;
 }
 
-input {
-  display: block;
+div input,
+div textarea {
+  font-family: "Roboto";
+  font-weight: lighter;
+  font-size: 16pt;
+  padding: 4px;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+div textarea {
+  resize: none;
+}
+
+form {
+  width: 100%;
+  margin: -5px 60px 0px 60px;
+}
+
+form div {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.share-line {
+  display: flex;
+  justify-content: space-between;
+}
+
+.share-line-item {
+  position: relative;
+  margin: 0px;
+  width: calc(50% - 3px);
+}
+
+.share-line-item input {
+  position: relative;
+}
+
+#comments {
+  height: 100px;
 }
 </style>
